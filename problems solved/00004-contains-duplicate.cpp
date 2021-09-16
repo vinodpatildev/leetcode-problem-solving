3d3d1fd6d234ac1b39e84bfaabf69cd131208685
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2021-09-16 17:38:02 +0530

    Leetcode problem Contains Duplicateis solved.
    Signed-off-by: Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>

---
+++ problems solved/00004-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> umap;
+        for(int i=0; i<nums.size(); i++){
+            umap[nums[i]]++;
+            if(umap[nums[i]]>=2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
