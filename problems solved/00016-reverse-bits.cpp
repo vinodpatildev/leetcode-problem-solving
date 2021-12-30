07a38d0e1193ed75c710bc226088707081ad8a27
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2021-12-30 23:50:42 +0530

    Leetcode problem Reverse Bitsis solved.
    Signed-off-by: Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>

---
+++ problems solved/00016-reverse-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0;
+        int i=32;
+        while(i){
+            ans = (ans<<1)+(n&1);
+            i--;
+            n = n>>1;
+        }
+        return ans;
+    }
+};
