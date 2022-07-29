1e47aa9516702ff2e95cb0abed250da568640b18
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2022-07-29 10:09:35 +0530

    Leetcode problem Min Stackis solved.
    Signed-off-by: Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>

---
+++ problems solved/00110-min-stack.cpp
@@ -0,0 +1,41 @@
+class MinStack {
+public:
+    stack<int> st,min_st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(min_st.empty()){min_st.push(val);}
+        else{
+            if(val<min_st.top()){
+                min_st.push(val);
+            }else{
+                min_st.push(min_st.top());
+            }
+        }
+    }
+    
+    void pop() {
+        st.pop();
+        min_st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return min_st.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
