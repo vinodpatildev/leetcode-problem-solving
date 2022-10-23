6e759f3644d6a6f94aa91742bff27763e63a6ae0
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2022-10-23 09:50:46 +0530

    Leetcode problem N-ary Tree Preorder Traversalis solved.
    Signed-off-by: Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>

---
+++ problems solved/00175-n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    void pre(Node* root, vector<int>& res){
+        res.push_back(root->val);
+        for(int i=0; i<root->children.size(); i++){
+            pre(root->children[i],res);
+        }
+    }
+    vector<int> preorder(Node* root) {
+        vector<int> res;
+        if(!root){return res;}
+        pre(root,res);
+        return res;
+    }
+};
