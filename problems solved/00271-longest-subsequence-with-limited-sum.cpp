bb3ff2abcd212f1e6d80e4cba514fd4d9f379730
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2022-12-25 19:18:04 +0530

    Leetcode problem Longest Subsequence With Limited Sumis solved.
    Signed-off-by: Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>

---
+++ problems solved/00271-longest-subsequence-with-limited-sum.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(nums.begin(),nums.end());
+        for(int i=1; i<nums.size(); i++) nums[i] += nums[i-1];
+        vector<int> res(queries.size());
+        for(int i=0; i<queries.size(); i++) res[i] = upper_bound(nums.begin(),nums.end(),queries[i]) - nums.begin();
+        return res;
+    }
+};
