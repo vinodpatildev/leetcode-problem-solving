778066bdc8df4926f1141b9198bff2a55b545bfd
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2023-01-17 10:47:45 +0530

    Leetcode problem Flip String to Monotone Increasingis solved.
    Signed-off-by: Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>

---
+++ problems solved/00301-flip-string-to-monotone-increasing.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        int flips=0,ones=0;
+        for(char ch : s){
+            if(ch == '0') flips++;
+            else ones++;
+            flips = min(flips,ones);
+        }
+        return flips;
+    }
+};
