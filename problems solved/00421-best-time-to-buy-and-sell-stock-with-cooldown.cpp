2db3d4a1699c42719d30b849544e0e6f4a17127e
Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>
2023-08-01 19:27:58 +0530

    Leetcode problem Best Time to Buy and Sell Stock with Cooldownis solved.
    Signed-off-by: Vinod Patil <61724808+vinodpatildev@users.noreply.github.com>

---
+++ problems solved/00421-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+	int maxProfit(vector<int>& prices){
+        int n = prices.size();
+        vector<vector<int>> dp(n+2,vector<int>(2));
+        dp[n][0] = dp[n][1] = dp[n+1][0] = dp[n+1][1] = 0;
+
+        for(int day=n-1; day>=0; day--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy) profit = max(-prices[day]+dp[day+1][0],dp[day+1][1]);
+                else profit = max(prices[day]+dp[day+2][1],dp[day+1][0]);
+                dp[day][buy] = profit;
+            }
+        }
+
+		return dp[0][1];
+	}
+};
